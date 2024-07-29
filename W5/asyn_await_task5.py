09a29385c37e24682b3a20a47ce7f07105e30c1a
Aektawan <s6506026220133@email.kmutnb.ac.th>
2024-07-29 22:13:56 +0700

    w5

--- W5/asyn_await_task5.py
+++ W5/asyn_await_task5.py
@@ -1,19 +1,19 @@
-import asyncio, time
+import asyncio,time
 
-async def ioioio(wela, chue_ngan):
-    print(f"เริ่ม%s เวลาผ่านไปแล้ว %.5f วินาที" (chue_ngan, time.time() - t0))
+async def ioioio(wela,chue_ngan):
+    print('Start%s Time has passed %.5f Second'%(chue_ngan,time.time()-t0))
     await asyncio.sleep(wela)
-    print(f"{chue_ngan} เสร็จแล้ว เวลาผ่านไปแล้ว %.51 วินาที (time.time() - t0)")
+    print('%sDone Time has passed %.5f Second'%(chue_ngan,time.time()-t0))
     return
 
 async def main():
-    print("Started Function")
-    phara1 = asyncio.create_task(ioioio(1.5, "Downloading Music"))
-    phara2 = asyncio.create_task(ioioio(2.5, "Downloading Anime"))
-    phara3 = asyncio.create_task(ioioio(0.5, "Downloading Movie"))
-    print("Mission Created")
+    print('Started function')
+    phara1 = asyncio.create_task(ioioio(1.5,'Downloading Music'))
+    phara2 = asyncio.create_task(ioioio(2.5,'Downloading Anime'))
+    phara3 = asyncio.create_task(ioioio(0.5,'Downloading Movie'))
+    print('Created Mission')
     await phara2
-    print("Mission Complete")
+    print('Function Complete')
 
 t0 = time.time()
 asyncio.run(main())
